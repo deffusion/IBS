43601316e5e41c516855a7bbb0dc106cfa835f9a
TrenlinHuang <1149674138@qq.com>
2023-03-21 09:50:31 +0800

    output packet

---
+++ output/packet.go
@@ -0,0 +1,57 @@
+package output
+
+import (
+	"IBS/information"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"strconv"
+)
+
+type Packet struct {
+	Id                  int
+	Timestamp           int64 `json:"timestamp"` // delay(μs) from the generation(timestamp) of information
+	PropagationDelay    int32 `json:"propagationDelay"`
+	TransmissionDelay   int32 `json:"transmissionDelay"`
+	QueuingDelaySending int32 `json:"queuingDelaySending"`
+	//QueuingDelayReceiving int32  `json:"queuingDelayReceiving"`
+	From       string `json:"from"`
+	To         string `json:"to"`
+	Hop        int    `json:"hop"`
+	Redundancy bool   `json:"redundancy"`
+}
+
+func newPacket(p *information.BasicPacket) *Packet {
+	return &Packet{
+		p.ID(),
+		p.Timestamp(),
+		p.PropagationDelay(),
+		p.TransmissionDelay(),
+		p.QueuingDelaySending(),
+		//p.QueuingDelayReceiving(),
+		strconv.FormatUint(p.From().Id(), 16),
+		strconv.FormatUint(p.To().Id(), 16),
+		p.Hop(),
+		p.Redundancy(),
+	}
+}
+
+type PacketOutput []*Packet
+
+func NewPacketOutput() *PacketOutput {
+	return &PacketOutput{}
+}
+func (o *PacketOutput) Append(p *information.BasicPacket) {
+	*o = append(*o, newPacket(p))
+}
+
+func (o *PacketOutput) WritePackets() {
+	b, err := json.Marshal(o)
+	if err != nil {
+		fmt.Println(err)
+	}
+	err = ioutil.WriteFile("output/output_packets.json", b, 0777)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
