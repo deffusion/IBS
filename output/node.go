cae663fb27565325a431cb4239ac03a2f13ad7c3
TrenlinHuang <1149674138@qq.com>
2023-03-21 09:32:28 +0800

    output node

---
+++ output/node.go
@@ -0,0 +1,48 @@
+package output
+
+import (
+	"IBS/node"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"strconv"
+)
+
+type Node struct {
+	Id                string `json:"id"`
+	Region            string `json:"region"`
+	DownloadBandwidth int    `json:"downloadBandwidth"` // byte/s
+	UploadBandwidth   int    `json:"uploadBandwidth"`
+	Running           bool   `json:"running"`
+}
+
+func newNode(n node.Node) *Node {
+	return &Node{
+		strconv.FormatUint(n.Id(), 16),
+		n.Region(),
+		n.DownloadBandwidth(),
+		n.UploadBandwidth(),
+		n.Running(),
+	}
+}
+
+type NodeOutput []*Node
+
+func NewNodeOutput() *NodeOutput {
+	return &NodeOutput{}
+}
+
+func (o *NodeOutput) Append(n node.Node) {
+	*o = append(*o, newNode(n))
+}
+
+func (o *NodeOutput) WriteNodes() {
+	b, err := json.Marshal(o)
+	if err != nil {
+		fmt.Println(err)
+	}
+	err = ioutil.WriteFile("output/output_nodes.json", b, 0777)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
